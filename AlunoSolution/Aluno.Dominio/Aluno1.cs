63ccd683b8b66cdc1768711a07a4a935c02b4259
Lucas Salles <sallespedrolucas380@gmail.com>
2025-08-29 21:12:08 -0300

    projeto

---
+++ AlunoSolution/Aluno.Dominio/Aluno1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Dominio
+{
+    public class Aluno1
+    {
+        public int Id { get; set; }
+        public string Nome {  get; set; }
+        public int Idade { get; set; }
+
+        public bool EhMaiordeIdade()
+        {
+            return Idade >= 18;
+        }
+    }
+}
---
+++ AlunoSolution/Aluno.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aluno.Dominio;
+
+namespace Aluno.Repositorio
+{
+    internal interface IAlunoRepositorio
+    {
+        void Adicionar(Aluno1 aluno);
+    }
+}
